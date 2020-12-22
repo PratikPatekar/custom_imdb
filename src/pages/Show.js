adff76aeadc0b861f9a49f8f0962894f8c4162ed
Pratik Patekar <pratik.patekar3@gmail.com>
2020-12-22 12:03:35 +0530

    Updated the Show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,17 +1,43 @@
-import React, { useEffect } from "react";
-import { useParams, useState } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { apiGet } from "../misc/config";
 
 const Show = () => {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=season&embed[]=cast`).then((results) => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=season&embed[]=cast`)
+      .then((results) => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
+
+  if (isLoading) {
+    return <div>Data is being Loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error Occured: {error}</div>;
+  }
 
   return <div>This is Show page</div>;
 };
